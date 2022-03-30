b284de7710692b68ff619a9f244f3aef109a1749
Moisés <moisesnetored@gmail.com>
2022-03-30 09:51:35 -0400

    feat: Paging logic and tweaks to data refresh in React Query.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,6 +19,7 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
@@ -28,7 +29,8 @@ import { Sidebar } from '../../components/Sidebar';
 import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error, refetch } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error, refetch } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -95,7 +97,7 @@ export default function UserList() {
                 </Thead>
 
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return (
                       <Tr key={user.id}>
                         <Td px={['4', '4', '6']}>
@@ -133,9 +135,10 @@ export default function UserList() {
               </Table>
 
               <Pagination
-                totalCountOfRegister={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegister={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+                // registersPerPage={0}
               />
             </>
           )}
